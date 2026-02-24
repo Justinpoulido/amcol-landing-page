bd4e5a297915fe0cb2fd1aec0344fbb4d3b553b8
Justinpoulido <Justinpoulido@users.noreply.github.com>
2026-02-24 16:58:39 -0400

    Added link to landing page and News

--- app/page.tsx
+++ app/page.tsx
@@ -331,7 +331,7 @@ export default function Home() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href="/News&Articles"
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-zinc-900 shadow-sm hover:bg-zinc-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
                 Read Latest News
